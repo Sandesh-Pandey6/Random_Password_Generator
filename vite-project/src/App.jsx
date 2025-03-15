b56eac942a6bd446c6b3c620b106492b119f8ef7
sandesh pandey <sandesh.pandey00112@gmail.com>
2025-03-15 17:49:35 +0545

    finally completed the random password generator app

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,13 +1,15 @@
-import { useState, useCallback } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useCallback, useEffect, useRef } from 'react'
+
 import './App.css'
 
 function App() {
-  const [length, setLength] = useState(8)
+  const [length, setLength] = useState(5)
   const[numberAllowed, setNumberAllowed] = useState(false)
   const[charAllowed, setCharAllowed] = useState(false)
-  const[password, setPassword] = useState("")
+  const[password, setPassword] = useState('')
+
+  const passwordRef = useRef(null)
+
   const passwordGenerator = useCallback(() =>{ 
     let pass = ""
     let str ="QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm"
@@ -18,9 +20,9 @@ function App() {
       str += "!@#$%^&*()_+"
     }
 
-    for(let i=1; i <= Array.length; i++) {
-      let char = Math.floor(Math.random()* str.length +1)
-      pass = str.charAt(char)
+    for(let i=1; i < length; i++) {
+      const char = Math.floor(Math.random()* str.length +1)
+      pass += str.charAt(char)
 
     }
 
@@ -33,18 +35,73 @@ function App() {
 
 
 
-  },[length, numberAllowed, charAllowed, setPassword])
+  },[length, numberAllowed, charAllowed])
+
+const copyPasswordToClipboard = useCallback(()=>{
+  window.navigator.clipboard.writeText(password)
+ passwordRef.current?.select()
+},[password])
+
+  useEffect(()=>{
+    passwordGenerator()
+  },[length, numberAllowed, charAllowed,passwordGenerator])
 
   return (
     < >
-      <div className='absolute top-0 left-0 w-full h-full shadow-md rounded-lg px-4 my-8 text-orange-400 bg-gray-700'>
-        <div className='className="flex shadow rounded-lg overflow-hidden mb-4 "'>
-          <input type="text" value={password}className='outline-none w-full py-1 px-3 bg-white' />
+      <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center min-h-screen bg-black">
+  <div className="relative w-full max-w-xl shadow-lg rounded-lg px-4 py-6 bg-gray-900">
+    <h1 className="text-3xl font-bold text-white mb-4">Password Generator</h1>
+    <div className="flex items-center shadow rounded-lg overflow-hidden">
+      <input 
+       
+        type="text" 
+        value={password}
+        className="outline-none w-full py-2 px-3 bg-white text-gray-500 text-lg rounded-lg shadow-md" 
+        placeholder="Password" 
+        readOnly 
+        ref={passwordRef}
+      /> 
 
-        </div>
+      <button onClick={copyPasswordToClipboard}
+      className='my-2 mx-2 font-bold'>copy</button>
 
+
+    </div>
+    <div className='flex text-sm gap-x-2'>
+      <div className='flex item-center ga--x-1'>
+        <input
+        type="range"
+        min={5}
+        max={100}
+        value={length}
+        className='cursor-pointer'
+        onChange={(e) => {setLength(e.target.value)}}
+        />
+        <label className='text-red-700'>length: {length}</label>
+      </div>
+      <div className='flex item-center gap-x-1'>
+        <input
+        type="checkbox"
+        defaultChecked={numberAllowed}
+        id="numberInput"
+        onChange={() =>{setNumberAllowed((prev)=>!prev);}}
+        />
+        <label className='text-red-700' htmlFor="NumperInput" >Number</label>
+      </div>
+      <div className='flex item-center gap-x-1'>
+        <input
+        type="checkbox"
+        defaultChecked={charAllowed}
+        id="charInput"
+        onChange={() =>{setCharAllowed((prev)=>!prev);}}
+        />
+        <label className='text-red-700' htmlFor="NumperInput" >Checkbox</label>
 
       </div>
+    </div>
+
+  </div>
+</div>
     </>
   )
 }
